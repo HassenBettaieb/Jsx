5072dd379b89f3ed94d1687bf5143de750466955
hassen <hsanbet5@gmail.com>
2021-10-19 17:07:13 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import pic from './pic.jpg'
+import './App.css';
+
+function App(){
+  
+    return (
+      <div>
+        <div style={{border: 'solid 1px black', maxWidth: '100vw'}}>
+          <h1 className="title red">Your name here</h1>
+          <br />
+          <img src={pic}  alt=" pic"/>
+          <br />
+          <img src="/images.jpg" alt="pic" />
+        </div>
+        <video width={320} height={240} controls>
+          <source src="myVideo.mp4" type="video/mp4" />
+        </video>
+      </div>
+    );
+  }
+export default App
